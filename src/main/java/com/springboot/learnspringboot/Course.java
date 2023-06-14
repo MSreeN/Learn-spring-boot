85e604bdf745a51f8ef08bd4c0d18384907fa583
MSreeN <khyathimnsree369@gmail.com>
2023-06-14 21:00:44 +0530

    resolved paranthesis error

--- src/main/java/com/springboot/learnspringboot/Course.java
+++ src/main/java/com/springboot/learnspringboot/Course.java
@@ -4,8 +4,9 @@ public class Course {
     private int courseId;
     private String courseName;
 
-    public Course(int courseId) {
+    public Course(int courseId, String courseName) {
         this.courseId = courseId;
+        this.courseName = courseName;
     }
 
     public int getCourseId() {
